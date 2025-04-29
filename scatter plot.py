16c9764f8ab4b363f3c64aee422ef85662e92a06
Aditya Kumar Tiwari <128886658+Adiiboy@users.noreply.github.com>
2025-04-29 22:19:16 +0530

    Update scatter plot.py

--- scatter plot.py
+++ scatter plot.py
@@ -1,40 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-# In[2]:
-
-
 df_bgmi=pd.read_csv (hash)
 df_bgmi
-
-
-# In[1]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
 df_bgmi=pd.read_csv("C:\\Users\\adity\\Documents\\archive\\Pubg_Stats.csv")
 df_bgmi
 
-
-# In[32]:
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 df_bgmi=pd.read_csv("C:\\Users\\adity\\Documents\\archive\\Pubg_Stats.csv",nrows = 100)
 df_bgmi  
-
-
-# In[35]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -43,10 +22,6 @@ x=df_bgmi["Matches_Played"]
 y= df_bgmi ["Kills"]
 plt.scatter(x,y)
 df_bgmi
-
-
-# In[36]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -57,10 +32,6 @@ plt.scatter(x,y)
 plt.xlabel("matched palyed")
 plt.ylabel("kills")
 df_bgmi
-
-
-# In[45]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -72,10 +43,6 @@ plt.xlabel("matched palyed")
 plt.ylabel("kills")
 plt.figure(figsize= (20,60))
 df_bgmi
-
-
-# In[52]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -87,10 +54,6 @@ plt.xlabel("matched palyed")
 plt.ylabel("kills")
 plt.figure(figsize= (20,60))
 df_bgmi
-
-
-# In[57]:
-
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -103,10 +66,3 @@ plt.xlabel("matched palyed")
 plt.ylabel("kills")
 plt.figure(figsize= (20,60))
 df_bgmi
-
-
-# In[ ]:
-
-
-
-
